a7480a262ad0f92cb0265f5e9a6872467ac03c02
Mondris <justmondris@gmail.com>
2021-07-25 19:28:07 +0100

    Add login dto and fields validations

--- src/main/java/com/aneeque/coding/challenge/demo/Dto/UserLoginDto.java
+++ src/main/java/com/aneeque/coding/challenge/demo/Dto/UserLoginDto.java
@@ -1,4 +1,24 @@
 package com.aneeque.coding.challenge.demo.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserLoginDto {
+
+    @NotBlank(message = "email is mandatory")
+    @Email
+    private String email;
+
+    @NotBlank(message = "password is mandatory")
+    private String password;
+
 }
--- src/main/java/com/aneeque/coding/challenge/demo/Dto/UserSignUpReqDto.java
+++ src/main/java/com/aneeque/coding/challenge/demo/Dto/UserSignUpReqDto.java
@@ -5,11 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.Column;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
