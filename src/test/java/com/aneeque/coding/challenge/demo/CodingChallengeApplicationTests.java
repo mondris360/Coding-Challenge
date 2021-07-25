a7ed079e3c8239082f0e93fc6e4f83b73d70ab31
Mondris <justmondris@gmail.com>
2021-07-25 18:33:34 +0100

    initial commit

---
+++ src/test/java/com/aneeque/coding/challenge/demo/CodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aneeque.coding.challenge.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
